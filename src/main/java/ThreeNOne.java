f2de381c237289aaeb662b7e7369310282cdb113
snoop <snoop@loafle.com>
2016-12-14 19:30:34 +0900

    first commit by 3n+1 tdd

---
+++ src/main/java/ThreeNOne.java
@@ -0,0 +1,5 @@
+/**
+ * Created by snoop on 16. 12. 14.
+ */
+public class ThreeNOne {
+}
---
+++ src/test/java/ThreeNOneTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by snoop on 16. 12. 14.
+ */
+public class ThreeNOneTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testFirst() throws Exception {
+
+
+        System.out.println("first!!!!");
+    }
+
+}
