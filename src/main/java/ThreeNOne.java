c516d7d28b6952cf0d78d1eb77d31092ebb5e42d
snoop <snoop@loafle.com>
2016-12-20 20:32:22 +0900

    end

--- src/main/java/ThreeNOne.java
+++ src/main/java/ThreeNOne.java
@@ -1,9 +1,117 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Created by snoop on 16. 12. 14.
  */
 public class ThreeNOne {
     public int start(int i, int i1) {
 
-        return 20;
+        List<Integer> fullList = this.fullList(i, i1);
+
+        int maxCycle = 0;
+        int currentCycle = 0;
+        for( int indexI = 0; indexI < fullList.size();++indexI) {
+            currentCycle = this.cycle(fullList.get(indexI));
+
+            if(currentCycle > maxCycle) {
+                maxCycle = currentCycle;
+            }
+        }
+
+        return maxCycle;
+    }
+
+    public int logic(int i) {
+
+        if(i % 2 == 0) {
+            return i/2;
+        }
+        else {
+            return i * 3 + 1;
+        }
+
+
+    }
+
+    public List<Integer> fullList(int i, int i1) {
+
+        List<Integer> returnList = new ArrayList<Integer>();
+        for( int indexI = i ; indexI <= i1; ++indexI) {
+            returnList.add(indexI);
+        }
+
+        return returnList;
+    }
+
+    public int cycle(int i) {
+
+        int cycle = 1;
+        int next = 0;
+
+        do {
+            next = this.logic(i);
+            i = next;
+            ++cycle;
+        }while(next != 1);
+
+
+
+
+        return cycle;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        ThreeNOne threeNOne = new ThreeNOne();
+
+       int[] intArray = threeNOne.inputs();
+
+        threeNOne.print(intArray[0], intArray[1]);
+    }
+
+    public int input() throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String value = bufferedReader.readLine();
+
+        int vint = Integer.valueOf(value);
+
+        return vint;
+
+    }
+
+    public int[] inputs() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String value = bufferedReader.readLine();
+
+        int[] retInts = new int[2];
+
+
+        String[] values = value.split(" ");
+
+        retInts[0] = Integer.valueOf(values[0]);
+        retInts[1] = Integer.valueOf(values[1]);
+
+
+
+        return retInts;
+
+    }
+
+    public void print(int i, int i1) {
+
+        int result = this.start(i, i1);
+
+
+        System.out.println(i + " " + i1 + " " + result);
+
+
     }
 }
--- src/test/java/ThreeNOneTest.java
+++ src/test/java/ThreeNOneTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -24,6 +26,62 @@ public class ThreeNOneTest {
         int result = t.start(1, 10);
 
         assertEquals(result, 20);
+
+
+        int logicResult = t.logic(10);
+
+        assertEquals(logicResult, 10/2);
+
+        logicResult = t.logic(11);
+
+        assertEquals(logicResult, 11 * 3 + 1);
+
+        List<Integer> fullList = t.fullList(1, 10);
+
+        assertEquals(fullList.size(), 10 + 1 - 1);
+
+
+        int cycle = t.cycle(22);
+
+        assertEquals(cycle, 16);
+
+        result = t.start(100, 200);
+        assertEquals(result, 125);
+
+
+        result = t.start(201, 210);
+        assertEquals(result, 89);
+
+
+        result = t.start(900, 1000);
+        assertEquals(result, 174);
+
+
+    }
+
+
+//    @Test
+    public void testInput() throws Exception {
+
+        ThreeNOne t = new ThreeNOne();
+
+        int result = t.input();
+
+        assertEquals(result, 1);
+
+    }
+
+    @Test
+    public void testPrint() throws Exception {
+
+
+        ThreeNOne t = new ThreeNOne();
+
+        t.print(1, 10);
+
+        t.print(100, 200);
+        t.print(201, 210);
+        t.print(900, 1000);
 
     }
 
